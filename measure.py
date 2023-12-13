0286e85a4a9e692c9160755427b2e578a370031d
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-12-13 23:19:05 +0100

    initial commit

---
+++ measure.py
@@ -0,0 +1,59 @@
+import smbus2
+import bme280
+import requests
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+port = 1
+address = 0x77
+bus = smbus2.SMBus(port)
+calibration_params = bme280.load_calibration_params(bus, address)
+server_url = os.getenv('URL')
+server_port= os.getenv('PORT')
+
+def get_measures():
+    calibration_params = bme280.load_calibration_params(bus, address)
+    data = bme280.sample(bus, address, calibration_params)
+
+    send_measures(
+        temperature=data.temperature,
+        pressure=data.pressure,
+        humidity=data.humidity,
+        measurementDate=data.timestamp.timestamp(),
+    )
+
+
+def send_measures(*, temperature, pressure, humidity, measurementDate):
+    url = f"{server_url}:{server_port}"
+    print("temperature: ", temperature)
+    variables = {
+        "temperature": temperature,
+        "pressure": pressure,
+        "humidity": humidity,
+        "measurementDate": measurementDate,
+        "measurementTable": "measurements"
+    }
+
+    body = """
+    mutation SaveMeasurements($temperature: Float, $pressure: Float, $humidity: Float, $measurementDate: Float, $measurementTable: String){
+        saveMeasurements(
+            temperature: $temperature
+            pressure: $pressure
+            humidity: $humidity
+            measurementDate: $measurementDate
+            measurementTable: $measurementTable
+        ) {
+            code
+            message
+        }
+    }
+    """
+
+    response = requests.post(url=url, json={"query": body, "variables": variables})
+    print(response.json())
+    time.sleep(25)
+    get_measures()
+
+get_measures()
---
+++ send_img.py
@@ -0,0 +1,57 @@
+from paramiko import SSHClient, AutoAddPolicy
+from scp import SCPClient
+from os import remove,getenv
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+server_url = os.getenv('URL')
+server_port= os.getenv('PORT')
+server_user= os.getenv('USER')
+server_path_to_pics= os.getenv('PATH_TO_PICS')
+
+remote={"host": server_url, "user": server_user, "path": server_path_to_pics}
+gql_url=f"{server_url}:{server_port}"
+
+def send_img_data(img_name:str)-> None:
+    variables = {
+        "title": img_name
+    }
+
+    body = """
+    mutation savePhotoData($title: String) {
+        savePhotoData(title: $title) {
+            code
+            message
+            }
+        }
+    """
+
+    response = requests.post(url=gql_url, json={"query": body, "variables": variables})
+    print(response.json())
+
+def send_img(path_tmp: str, img_name: str, current_date: str)-> None:
+    img_path = path_tmp + img_name
+    try:
+        ssh = SSHClient()
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(AutoAddPolicy())
+        ssh.connect(hostname=remote["host"], username=remote["user"])
+        scp = SCPClient(ssh.get_transport())
+        scp.put(img_path, remote_path=remote["path"])
+        print("Photo has been sent")
+        send_img_data(current_date)
+        remove_img_from_tmp(img_path)
+
+    except Exception as e:
+        print("Scp send error", e)
+        sleep(2) 
+        send_img(img_path)
+
+def remove_img_from_tmp(img_path:str)-> None:
+    try:
+       remove(img_path) 
+       print("File has been removed")
+       
+    except:
+       print("File removal error")  
