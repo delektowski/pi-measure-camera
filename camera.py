af90f4594eaa806e975c6656a96a9de4da620322
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-12-17 22:49:39 +0100

    add semaphore

--- camera.py
+++ camera.py
@@ -64,4 +64,7 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass  # Handle Ctrl+C gracefully
--- send_img.py
+++ send_img.py
@@ -14,6 +14,8 @@ server_path_to_pics = getenv("PATH_TO_PICS")
 remote = {"host": server_url, "user": server_user, "path": server_path_to_pics}
 gql_url = f"{server_url}:{server_port}"
 
+semaphore = asyncio.Semaphore(2) 
+
 
 async def send_img_data(img_name: str) -> None:
     variables = {"title": img_name}
@@ -36,22 +38,23 @@ async def send_img_data(img_name: str) -> None:
 
 async def send_img(path_tmp: str, img_name: str, current_date: str) -> None:
     img_path = path_tmp + img_name
-    try:
-        ssh = SSHClient()
-        ssh.load_system_host_keys()
-        ssh.set_missing_host_key_policy(AutoAddPolicy())
-        ssh.connect(hostname=remote["host"], username=remote["user"])
-        scp = SCPClient(ssh.get_transport())
-        scp.put(img_path, remote_path=remote["path"])
-        asyncio.sleep(2)
-        print("Photo has been sent.")
-        await remove_img_from_tmp(img_path)
-        await send_img_data(current_date)
+    async with semaphore:
+        try:
+            ssh = SSHClient()
+            ssh.load_system_host_keys()
+            ssh.set_missing_host_key_policy(AutoAddPolicy())
+            ssh.connect(hostname=remote["host"], username=remote["user"])
+            scp = SCPClient(ssh.get_transport())
+            scp.put(img_path, remote_path=remote["path"])
+            asyncio.sleep(2)
+            print("Photo has been sent.")
+            await remove_img_from_tmp(img_path)
+            await send_img_data(current_date)
 
-    except Exception as err:
-        print("Scp send error: ", err)
-        await asyncio.sleep(2)
-        await send_img(img_path, img_name, current_date)
+        except Exception as err:
+            print("Scp send error: ", err)
+            await asyncio.sleep(2)
+            await send_img(img_path, img_name, current_date)
 
 
 async def remove_img_from_tmp(img_path: str) -> None:
