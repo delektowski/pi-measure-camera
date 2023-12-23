f1046b5a1c04548e026ecaae77acf73d612608ee
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-12-24 00:33:28 +0100

    Add asyncssh
    - remove scp
    - remove semaphore

--- send_img.py
+++ send_img.py
@@ -1,9 +1,9 @@
 from paramiko import SSHClient, AutoAddPolicy
-from scp import SCPClient
 from os import remove, getenv
 from dotenv import load_dotenv
 import requests
 import asyncio
+import asyncssh
 
 load_dotenv()
 server_url = getenv("URL")
@@ -14,7 +14,6 @@ server_path_to_pics = getenv("PATH_TO_PICS")
 remote = {"host": server_url, "user": server_user, "path": server_path_to_pics}
 gql_url = f"{server_url}:{server_port}"
 
-semaphore = asyncio.Semaphore(4) 
 
 
 async def send_img_data(img_name: str) -> None:
@@ -38,23 +37,20 @@ async def send_img_data(img_name: str) -> None:
 
 async def send_img(path_tmp: str, img_name: str, current_date: str) -> None:
     img_path = path_tmp + img_name
-    async with semaphore:
-        try:
-            ssh = SSHClient()
-            ssh.load_system_host_keys()
-            ssh.set_missing_host_key_policy(AutoAddPolicy())
-            ssh.connect(hostname=remote["host"], username=remote["user"])
-            scp = SCPClient(ssh.get_transport())
-            scp.put(img_path, remote_path=remote["path"])
-            await asyncio.sleep(2)
-            print("Photo has been sent.")
-            await remove_img_from_tmp(img_path)
-            await send_img_data(current_date)
-
-        except Exception as err:
-            print("Scp send error: ", err)
-            await asyncio.sleep(2)
-            await send_img(img_path, img_name, current_date)
+   
+    try:
+        async with asyncssh.connect(remote['host'], username=remote['user']) as conn:
+            async with conn.start_sftp_client() as sftp:
+                await sftp.put(img_path, remote["path"])
+                await asyncio.sleep(2)
+                print("Photo has been sent.")
+                await remove_img_from_tmp(img_path)
+                await send_img_data(current_date)
+    
+    except Exception as err:
+        print("Sftp send error: ", err)
+        await asyncio.sleep(2)
+        await send_img(img_path, img_name, current_date)
 
 
 async def remove_img_from_tmp(img_path: str) -> None:
