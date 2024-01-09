c36fabf2bf360ad0481b4ee471e60b02bb619e00
Marcin Delektowski <marcin.delektowski@gmail.com>
2024-01-09 21:19:40 +0100

    formating cleanup

--- send_img.py
+++ send_img.py
@@ -3,20 +3,16 @@ from os import remove, getenv, path
 from dotenv import load_dotenv
 import requests
 import asyncio
-import asyncssh
 import logging
 
-logging.basicConfig(filename='./logs/errors.log', encoding='utf-8', level=logging.ERROR)
+logging.basicConfig(filename="./logs/errors.log", encoding="utf-8", level=logging.ERROR)
 
 load_dotenv()
 server_url = getenv("URL")
 server_port = getenv("PORT")
 server_path_to_pics = getenv("PATH_TO_PICS")
 
-
 gql_url = f"{server_url}:{server_port}"
-
-
 
 async def send_img_data(img_name: str) -> None:
     variables = {"title": img_name}
@@ -37,30 +33,32 @@ async def send_img_data(img_name: str) -> None:
     await asyncio.sleep(1)
 
 
-async def send_img(path_tmp: str, img_name: str, current_date: str, attempt = 0) -> None:
+async def send_img(path_tmp: str, img_name: str, current_date: str, attempt=0) -> None:
     img_path = path_tmp + img_name
-   
+
     try:
-        proc = await asyncio.create_subprocess_exec('scp', img_path, server_path_to_pics,
-        stderr=asyncio.subprocess.PIPE)
+        proc = await asyncio.create_subprocess_exec(
+            "scp", img_path, server_path_to_pics, stderr=asyncio.subprocess.PIPE
+        )
 
         _, stderr = await proc.communicate()
-        
+
         if proc.returncode == 0:
             print("Photo has been sent.")
-            await remove_img_from_tmp(path_tmp,img_name)
+            await remove_img_from_tmp(path_tmp, img_name)
             await send_img_data(current_date)
         if stderr:
             raise Exception(stderr.decode())
-    
+
     except Exception as err:
         print("Img send error: ", err)
         await asyncio.sleep(2)
-        logging.error()('LOG ERROR in send_img: ')
+        logging.error()("LOG ERROR in send_img: ")
         if attempt < 10:
             await send_img(img_path, img_name, current_date, attempt + 1)
 
-async def remove_img_from_tmp(path_tmp: str,img_name: str ) -> None:
+
+async def remove_img_from_tmp(path_tmp: str, img_name: str) -> None:
     try:
         file_path = path.join(path_tmp, img_name)
         remove(file_path)
